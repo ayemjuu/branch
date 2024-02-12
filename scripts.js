4a85ce30bcf4090b21ab155701c5cf2b3874f8c8
ayemjuu <kaijuuuuu10@gmail.com>
2024-02-12 16:51:12 +0800

    first commit

---
+++ scripts.js
@@ -0,0 +1,16 @@
+const loginBtn = document.getElementById('loginBtn');
+const signupBtn = document.getElementById('signupBtn');
+const loginForm = document.getElementById('loginForm');
+const signupForm = document.getElementById('signupForm');
+const submitLoginBtn = document.getElementById('submitLogin');
+const submitSignupBtn = document.getElementById('submitSignup');
+
+loginBtn.addEventListener('click', () => {
+    loginForm.classList.remove('hidden');
+    signupForm.classList.add('hidden');
+});
+
+signupBtn.addEventListener('click', () => {
+    signupForm.classList.remove('hidden');
+    loginForm.classList.add('hidden');
+});
